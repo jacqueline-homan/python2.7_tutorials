34661393e1c849fafe8ce299a023494bc2b08687
Jacqueline Homan <jacquelinehoman7@gmail.com>
2015-11-30 08:17:48 -0500

    Added file utils examples and modified text file

--- IO.py
+++ IO.py
@@ -1,10 +1,10 @@
 def main():
 # Open a file for writing and create it if it doesn't exist
-#	f = open("textfile.txt", "w+")
-# Write 10 lines of text and the line numbers to the file
-#	for i in range(10):
-#		f.write("This is line  %d\n\r" % (i+1))
-# Open the file to append data to the end
+	f = open("textfile.txt", "w+")
+#Write 10 lines of text and the line numbers to the file
+	for i in range(10):
+		f.write("This is line  %d\n\r" % (i+1))
+ #Open the file to append data to the end
 	f = open("textfile.txt", "a+")
 	for i in range(10):
 		f.write("And this line %d\n\r" % (i+1))
--- file_utils.py
+++ file_utils.py
@@ -4,11 +4,25 @@ import datetime
 from datetime import date, time, timedelta
 import time
  # Check if file exists
-print os.name;
-print "File exists: " + str(path.exists("textfile.txt"))
-print "File is a file: " + str(path.isfile("textfile.txt"))
-print "File is a directory: " + str(path.isdir("textfile.txt"))
+def main():
+	print os.name;
+	print "File exists: " + str(path.exists("textfile.txt"))
+	print "File is a file: " + str(path.isfile("textfile.txt"))
+	print "File is a directory: " + str(path.isdir("textfile.txt"))
 
-# Work with file paths
-print "File path: " + str(path.realpath("textfile.txt"))
-print "File path and name: " + str(path.split(path.realpath("textfile.txt")))
+	# Work with file paths
+	print "File path: " + str(path.realpath("textfile.txt"))
+	print "File path and name: " + str(path.split(path.realpath("textfile.txt")))
+
+	# Get modification times
+	t = time.ctime(path.getmtime("textfile.txt"))
+	print t
+	print datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+
+	# Compute how long it's been since file has been modified
+	td= datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+	print "It's been " + str(td) + " since file was modified"
+	print "Or, if you prefer, " + str(td.total_seconds()) + " seconds since it was modified (just sayin')"
+
+if __name__ == "__main__":
+	main()
