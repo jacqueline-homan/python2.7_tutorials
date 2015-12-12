d1ed34880afa82409e9c9f32103b55fe7c2393b9
Jacqueline Homan <jacquelinehoman7@gmail.com>
2015-12-11 21:59:03 -0500

    Added bak file

--- shell_utils.py
+++ shell_utils.py
@@ -1,4 +1,26 @@
+import os
+import shutil
+from os import path
+from shutil import make_archive
+from zipfile import ZipFile
+
 def main():
+# Make a copy or duplicate of an existing file
+	if path.exists("textfile.txt"):
+		# Get the path of the file in the current directory
+		src = path.realpath("textfile.txt")
+		# Separate the path part from the filename
+		head, tail = path.split(src)
+		print "path: " + head
+		print "file: " + tail
+
+		# Now, make a backup file
+		dst = src + ".bak"
+		# Then use the shell to make a copy of the file
+		shutil.copy(src,dst)
+		# If you want to copy over perms, modification times, and other datta
+		shutil.copystat(src,dst)
+
 
 	
 
